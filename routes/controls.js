7cbe067e55952cdfbad3a4b6e16ac4d7d53404d4
talented_vicky <victorotubure7@gmail.com>
2023-11-23 18:24:39 +0100

    improved on the dashboard page and completed backend logic

--- routes/controls.js
+++ routes/controls.js
@@ -3,27 +3,35 @@ const axios = require('axios')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    // just display landing page
+router.get('/', (req, res) => {
     res.render('landing_page', {
         title: 'Home'
     })
 });
 
-router.get('/user/profile', async (req, res, next) => {
-    req.body = { EnvironmentSatisfaction, JobSatisfaction, WorkLifeBalance, Age,
-    BusinessTravel, Department, DistanceFromHome, Education, Gender, JobLevel, 
+router.post('/user/profile', async (req, res) => {
+    const { Age, Gender, EnvironmentSatisfaction, JobSatisfaction, WorkLifeBalance,
+    BusinessTravel, Department, DistanceFromHome, Education, JobLevel, 
     JobRole, MaritalStatus, MonthlyIncome, NumCompaniesWorked, PercentSalaryHike, 
     TotalWorkingYears, YearsAtCompany, YearsSinceLastPromotion, YearsWithCurrManager, 
-    JobInvolvement, PerformanceRating };
+    JobInvolvement, PerformanceRating } = req.body;
+
+    console.log(Age)
+    console.log(EnvironmentSatisfaction)
     
     const apimodel = "https://employ-api.onrender.com/predict/";
     try {
-        const response = await axios.post(apimodel, {EnvironmentSatisfaction, JobSatisfaction, WorkLifeBalance, Age,
-            BusinessTravel, Department, DistanceFromHome, Education, Gender, JobLevel, 
-            JobRole, MaritalStatus, MonthlyIncome, NumCompaniesWorked, PercentSalaryHike, 
-            TotalWorkingYears, YearsAtCompany, YearsSinceLastPromotion, YearsWithCurrManager, 
-            JobInvolvement, PerformanceRating});
+        const response = await axios.post(apimodel, {
+            EnvironmentSatisfaction: Number(EnvironmentSatisfaction), JobSatisfaction: Number(JobSatisfaction), 
+            WorkLifeBalance: Number(WorkLifeBalance), Age: Number(Age), BusinessTravel: Number(BusinessTravel), 
+            Department: Number(Department), DistanceFromHome: Number(DistanceFromHome), 
+            Education: Number(Education), Gender: Number(Gender), JobLevel: Number(JobLevel), 
+            JobRole: Number(JobRole), MaritalStatus: Number(MaritalStatus), MonthlyIncome:  Number(MonthlyIncome), 
+            NumCompaniesWorked: Number(NumCompaniesWorked), PercentSalaryHike: Number(PercentSalaryHike), 
+            TotalWorkingYears: Number(TotalWorkingYears), YearsAtCompany: Number(YearsAtCompany), 
+            YearsSinceLastPromotion: Number(YearsSinceLastPromotion), YearsWithCurrManager: Number(YearsWithCurrManager), 
+            JobInvolvement: Number(JobInvolvement), PerformanceRating: Number(PerformanceRating)
+        });
     
         console.log(response)
         if(!response){
@@ -31,16 +39,16 @@ router.get('/user/profile', async (req, res, next) => {
             error.statusCode = 500;
             throw error
         }
+        const value = response.body
 
         res.render('result', {
-            title: "Profile Result",
-            tis: '',
-            fin: 'sth'
+            title: "Result", value
         })
     } catch (error) {
         console.log(error)
         res.status(500).json({
-            error: error.message, msg: "Error fetching api", info: error.info,
+            message: "Error posting to API", 
+            info: error.message,
         })
     }
 });
